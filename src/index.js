afffc6a6bf414f96a2a0e4376293e8be67dc7d59
avinashrahul.gudimet <avinashrahul.gudimet@dish.com>
2017-10-03 10:37:52 -0600

    Basic React router setup

--- src/index.js
+++ src/index.js
@@ -2,14 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+// SPA's contains one HTML file and navigate within for every user action instead of calling server.
+class Hello extends React.Component {
+  render() {
+    return(
+        <div>
+          Hello!
+        </div>
+      );
+  }
+}
+
+class Goodbye extends React.Component {
+  render() {
+    return(
+        <div>
+          Goodbye!
+        </div>
+      );
+  }
+}
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Hello} />
+        <Route path="/goodbye" component={Goodbye} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
